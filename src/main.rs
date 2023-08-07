a3ef802ad3165453c0b1318d379ef7f4ef334b2a
Aoife Bradley <me@ifa.codes>
2023-08-07 17:35:32 -0400

    add tumblr.rs
    Former-commit-id: b5ffabaf876a49f4473606b307a51b67ac147448

--- src/main.rs
+++ src/main.rs
@@ -1,293 +1,93 @@
+mod tumblr;
+
 use anyhow::*;
+use env_logger::Env;
 use futures::*;
 use log::*;
-use reqwest::{Client, Response};
+use reqwest::{Client, StatusCode};
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
-use std::{borrow::Cow, collections::HashMap, error::Error};
-use tokio::{
-    fs::{File, OpenOptions},
-    io::AsyncWriteExt,
-};
-
+use thiserror::Error;
+use tokio_stream::StreamExt;
+use tumblr::*;
 use url::Url;
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Blog {
-    name: String,
-    title: String,
-    url: Url,
-    description: String,
-    uuid: String,
-    updated: usize,
-    can_show_badges: bool,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(tag = "type")]
-#[serde(rename_all = "lowercase")]
-enum Post {
-    Blocks {
-        is_blocks_post_format: bool,
-        blog_name: String,
-        blog: Blog,
-        id: usize,
-        id_string: String,
-        is_blazed: bool,
-        is_blaze_pending: bool,
-        post_url: Url,
-        slug: String,
-        date: String,
-        timestamp: usize,
-        state: String,
-        reblog_key: String,
-        tags: Vec<String>,
-        short_url: Url,
-        summary: String,
-        should_open_in_legacy: bool,
-        // recommended_source: Option<>,
-        // recommened_color: Option<>,
-        note_count: usize,
-        // content: Vec<Content>,
-        // layout: Vec<Layout>,
-        // trail: Vec<Trail>,
-        liked_timestamp: usize,
-        can_like: bool,
-        interactability_reblog: String,
-        interactability_blaze: String,
-        can_reblog: bool,
-        can_send_in_message: bool,
-        can_reply: bool,
-        display_avatar: bool,
-    },
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(tag = "type")]
-#[serde(rename_all = "lowercase")]
-enum Content {
-    Text {
-        text: String,
-        subtype: Option<String>,
-        indent_level: Option<usize>,
-        formatting: Vec<InlineFormat>,
-    },
-    Image {
-        media: Vec<MediaObject>,
-        colors: Option<HashMap<String, Value>>,
-        feedback_token: Option<String>,
-        poster: Option<Poster>,
-        alt_text: Option<String>,
-        caption: Option<String>,
-    },
-    Link {
-        url: Url,
-        title: String,
-        description: String,
-        author: String,
-        site_name: Option<String>,
-        display_url: Option<String>,
-        poster: Option<Poster>,
-    },
-    Audio {
-        url: Option<Url>,
-        media: Box<Option<MediaObject>>,
-        provider: Option<String>,
-        title: Option<String>,
-        artist: Option<String>,
-        album: Option<String>,
-        poster: Option<Poster>,
-        embed_html: Option<String>,
-        embed_url: Option<Url>,
-    },
-    Video {
-        url: Option<Url>,
-        media: Box<Option<MediaObject>>,
-        provider: Option<String>,
-        embed_html: Option<String>,
-        embed_iframe: Option<EmbedIFrameObject>,
-        embed_url: Option<Url>,
-        poster: Option<Poster>,
-        can_autoplay_on_cellular: Option<String>,
-    },
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(tag = "type")]
-#[serde(rename_all = "lowercase")]
-enum InlineFormat {
-    Bold(Range),
-    Italic(Range),
-    StrikeThrough(Range),
-    Small(Range),
-    Link {
-        #[serde(flatten)]
-        range: Range,
-        url: Url,
-    },
-    Mention {
-        #[serde(flatten)]
-        range: Range,
-        blog: Blog,
-    },
-    Color {
-        #[serde(flatten)]
-        range: Range,
-        hex: String,
-    },
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Range {
-    start: usize,
-    end: usize,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Poster {
-    #[serde(rename = "type")]
-    ty: String,
-    url: Url,
-    widht: usize,
-    height: usize,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Layout {
-    #[serde(rename = "type")]
-    ty: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Trail {
-    #[serde(rename = "type")]
-    ty: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct MediaObject {
-    media_key: String,
-    url: Url,
-    #[serde(rename = "type")]
-    ty: Option<String>,
-    width: Option<usize>,
-    height: Option<usize>,
-    original_dimensions_missing: Option<bool>,
-    cropped: Option<bool>,
-    has_original_dimensions: Option<bool>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct EmbedIFrameObject {
-    url: Url,
-    width: usize,
-    height: usize,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Meta {
-    status: usize,
-    msg: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct TumblrResponse {
-    meta: Meta,
-    response: Option<Liked>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Liked {
-    liked_posts: Vec<Post>,
-    liked_count: usize,
-    #[serde(rename = "_links")]
-    pagination: Pagination,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Pagination {
-    next: Option<PaginationResult>,
-    prev: Option<PaginationResult>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct PaginationResult {
-    href: String,
-    method: String,
-    query_params: QueryParams,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct QueryParams {
-    npf: Option<String>,
-    before: Option<String>,
-    after: Option<String>,
-}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    env_logger::init();
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
+    let posts = tokio::fs::read("posts.json").await?;
+    let mut posts: Vec<Post> = serde_json::from_slice(&posts)?;
 
     let stream = get_likes(None).await;
+    pin_mut!(stream);
+    let new_posts: Vec<Post> = stream
+        .as_mut()
+        .take_while(Result::is_ok)
+        .try_collect()
+        .await?;
+    posts.extend(new_posts);
 
-    let posts: Vec<Post> = stream.collect().await; //println!("{}", serde_json::to_string_pretty(&res_json.response)?);
+    if let Some(Err(err)) = stream.next().await {
+        if let Some(ApiError::RateLimit { timestamp, .. }) = err.downcast_ref::<ApiError>() {
+            if let Some(timestamp) = timestamp {
+                info!("last returned timestamp: {}", timestamp);
+            } else {
+                info!("no final timestamp returned!");
+            }
+        }
+    }
 
-    tokio::fs::write("posts.json", serde_json::to_string_pretty(&posts)?).await?;
+    tokio::fs::write(
+        "posts.json",
+        serde_json::to_string_pretty(posts.as_slice())?,
+    )
+    .await?;
 
     Ok(())
 }
 
-async fn get_likes(before: Option<String>) -> impl Stream<Item = Post> {
-    stream::unfold(before, |token| async move {
+async fn get_likes(before: Option<String>) -> impl Stream<Item = Result<Post>> {
+    stream::try_unfold(before, |token| async move {
+        //debug!("{token:?}");
         let mut query = vec![
             (
                 "api_key".to_string(),
                 "yWH70O23rRJzAO69e6nj0lRUdrU7iCs8hCiUJZ6V7SM4TZGxIf".to_string(),
             ),
+            //("limit".to_string(), "1".to_string()),
             ("npf".to_string(), "true".to_string()),
         ];
-        if let Some(time) = token {
-            query.push(("before".to_string(), time))
+        if let Some(time) = &token {
+            query.push(("before".to_string(), time.to_string()))
         };
         let client = Client::new();
-        client
+        let resp = client
             .get("https://api.tumblr.com/v2/blog/manxome-foe/likes")
             .query(query.as_slice())
             .send()
-            .map_err(anyhow::Error::from)
-            .and_then(|res| async move {
-                let status = res.status();
-                res.bytes()
-                    .map_err(anyhow::Error::from)
-                    .and_then(|res_bytes| async move {
-                        //let _ = tokio::fs::write("response2.json", &res_bytes).await;
-                        if status.is_success() {
-                            debug!("Status Returned: {}", status);
-                            //info!("{:?}", json.response);
-                            serde_json::from_slice::<TumblrResponse>(&res_bytes)
-                                .map_err(|e| {
-                                    error!("{:?}", &e);
-                                    anyhow::Error::from(e)
-                                })
-                                .and_then(|tr| tr.response.context("no response"))
-                        } else {
-                            error!("Status Returned: {}", status);
-                            bail!("ohno")
-                        }
-                    })
-                    .map_ok(|liked| {
-                        let before = if let Some(next) = liked.pagination.next {
-                            next.query_params.before
-                        } else {
-                            None
-                        };
-                        info!("{:?}", before);
-                        (stream::iter(liked.liked_posts), before)
-                    })
-                    .await
-            })
-            .await
-            .ok()
+            .inspect_err(|err| error!("{err:?}"))
+            .await?;
+        let status = resp.status();
+        if !status.is_success() {
+            error!("status returned: {status}");
+            return Err(anyhow!(ApiError::RateLimit {
+                status,
+                timestamp: token,
+            }));
+        }
+        let resp: TumblrResponse = resp.json().inspect_err(|err| error!("{err:?}")).await?;
+        let resp = resp
+            .response
+            .context("no likes were returned with the response")?;
+        let next_timestamp = resp
+            .pagination
+            .next
+            .and_then(|next| next.query_params.before);
+        debug!("next timestamp: {next_timestamp:?}");
+        Ok(Some((
+            stream::iter(resp.liked_posts.into_iter().map(Ok)),
+            next_timestamp,
+        )))
     })
-    .flatten()
+    .try_flatten()
 }
